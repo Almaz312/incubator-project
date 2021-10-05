13e7cc40ee136a7571024cca542429bad3323fc1
deadpooll312 <deadpoll312@gmail.com>
2021-10-05 21:34:35 +0600

    task-75

--- src/pages/freeCodeCamp/task_5_10_2021.js
+++ src/pages/freeCodeCamp/task_5_10_2021.js
@@ -67,3 +67,10 @@ var lastName = "Lovelace";
 // Only change code below this line
 
 lastNameLength = lastName.length;
+
+// Setup
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
