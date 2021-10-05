e539e753c0fe6fa9ea0f96e2164d98d2e1acc851
deadpooll312 <deadpoll312@gmail.com>
2021-10-05 23:53:28 +0600

    task 154

--- src/pages/freeCodeCamp/task_5_10_2021.js
+++ src/pages/freeCodeCamp/task_5_10_2021.js
@@ -150,3 +150,9 @@ function reusableFunction() {
     console.log("Hi World");
 }
 reusableFunction();
+
+function functionWithArgs(param1, param2) {
+    console.log(param1 + param2);
+}
+functionWithArgs(1,2);
+functionWithArgs(7,9);
