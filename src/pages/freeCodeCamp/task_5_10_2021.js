633ed47247115f14a133e7896f037a8d118c899c
deadpooll312 <deadpoll312@gmail.com>
2021-10-05 22:40:28 +0600

    task 124

--- src/pages/freeCodeCamp/task_5_10_2021.js
+++ src/pages/freeCodeCamp/task_5_10_2021.js
@@ -119,3 +119,8 @@ myArray[0] = 45;
 var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
 
 var myData = myArray[2][1];
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+myArray.push(["dog", 3])
+// Only change code below this line
