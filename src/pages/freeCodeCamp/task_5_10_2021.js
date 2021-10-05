8bdac5a6d630fd72a7e2eae7090f81a2917d2947
deadpooll312 <deadpoll312@gmail.com>
2021-10-05 20:24:49 +0600

    task-54

--- src/pages/freeCodeCamp/task_5_10_2021.js
+++ src/pages/freeCodeCamp/task_5_10_2021.js
@@ -49,3 +49,7 @@ var myStr = "This is the start. " + "This is the end."; // Change this line
 
 var myStr = "This is the first sentence. ";
 myStr += "This is the second sentence.";
+
+// Only change code below this line
+var myName = "freeCodeCamp";
+var myStr = "My name is " + myName + "and I am well!";
