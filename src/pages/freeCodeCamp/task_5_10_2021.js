0e7b12abf5e30322ad4325c663e5fbb85a237a52
deadpooll312 <deadpoll312@gmail.com>
2021-10-05 22:58:00 +0600

    task 129

--- src/pages/freeCodeCamp/task_5_10_2021.js
+++ src/pages/freeCodeCamp/task_5_10_2021.js
@@ -124,3 +124,9 @@ var myData = myArray[2][1];
 var myArray = [["John", 23], ["cat", 2]];
 myArray.push(["dog", 3])
 // Only change code below this line
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+var removedFromMyArray = myArray.pop();
