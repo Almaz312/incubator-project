edb763c40ecdb8a43a5697e556314f6ca949280a
deadpooll312 <deadpoll312@gmail.com>
2021-10-05 19:58:00 +0600

    task-42

--- src/pages/freeCodeCamp/task_5_10_2021.js
+++ src/pages/freeCodeCamp/task_5_10_2021.js
@@ -38,3 +38,5 @@ let myFirstName = "Vasya"
 let myLastName = "Alibabaevich"
 
 var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
