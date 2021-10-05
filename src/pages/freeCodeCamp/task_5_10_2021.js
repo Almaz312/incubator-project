2c12f36171f6ae52c20a4f7c11116757fc00bf7c
deadpooll312 <deadpoll312@gmail.com>
2021-10-05 21:56:07 +0600

    task 100

--- src/pages/freeCodeCamp/task_5_10_2021.js
+++ src/pages/freeCodeCamp/task_5_10_2021.js
@@ -93,3 +93,9 @@ var lastName = "Lovelace";
 
 // Only change code below this line
 var lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
