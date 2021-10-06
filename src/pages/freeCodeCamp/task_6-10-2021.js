c63cc82359e5adb58fd813ecb43d7722eaa5c57a
deadpooll312 <deadpoll312@gmail.com>
2021-10-06 20:45:20 +0600

    task 21

--- src/pages/freeCodeCamp/task_6-10-2021.js
+++ src/pages/freeCodeCamp/task_6-10-2021.js
@@ -16,3 +16,20 @@ function timesFive(num) {
     return num * 5;
 }
 var answer = timesFive(5)
+
+// Setup
+var sum = 0;
+
+function addThree() {
+    sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive() {
+    sum = sum + 5;
+}
+
+// Only change code above this line
+
+addThree();
+addFive();
