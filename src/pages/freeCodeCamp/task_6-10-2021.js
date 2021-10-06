b7101b408a18a6b1873c9471342e2959ac88afa8
zamla312 <zamlazhekshenov@gmail.com>
2021-10-07 00:14:04 +0600

    task 190

--- src/pages/freeCodeCamp/task_6-10-2021.js
+++ src/pages/freeCodeCamp/task_6-10-2021.js
@@ -186,3 +186,15 @@ function testLessOrEqual(val) {
 }
 
 testLessOrEqual(10);
+
+function testLogicalAnd(val) {
+    // Only change code below this line
+
+    if (val >= 25 && val <= 50) {
+        return "Yes";
+    }
+    // Only change code above this line
+    return "No";
+}
+
+testLogicalAnd(10);
