edd41dfd16ac0534e627ee4b6155e67139a39a21
zamla312 <zamlazhekshenov@gmail.com>
2021-10-06 21:40:06 +0600

    task 64

--- src/pages/freeCodeCamp/task_6-10-2021.js
+++ src/pages/freeCodeCamp/task_6-10-2021.js
@@ -60,3 +60,12 @@ var testArr = [1,2,3,4,5];
 console.log("Before: " + JSON.stringify(testArr));
 console.log(nextInLine(testArr, 6));
 console.log("After: " + JSON.stringify(testArr));
+
+function welcomeToBooleans() {
+
+    // Only change code below this line
+
+    return true; // Change this line
+
+    // Only change code above this line
+}
