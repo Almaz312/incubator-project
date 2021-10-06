253e4ffeb952502a88d1ce3a3fa290f4d5422601
deadpooll312 <deadpoll312@gmail.com>
2021-10-06 20:31:53 +0600

    task 1-2

--- src/pages/freeCodeCamp/task_6-10-2021.js
+++ src/pages/freeCodeCamp/task_6-10-2021.js
@@ -4,7 +4,7 @@ var outerWear = "T-Shirt";
 function myOutfit() {
     // Only change code below this line
 
-
+    var outerWear = "sweater"
 
     // Only change code above this line
     return outerWear;
