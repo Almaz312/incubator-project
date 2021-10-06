c5dfb4a2d330d1f08486818e729342b5e34d00ad
zamla312 <zamlazhekshenov@gmail.com>
2021-10-06 21:38:20 +0600

    task 57

--- src/pages/freeCodeCamp/task_6-10-2021.js
+++ src/pages/freeCodeCamp/task_6-10-2021.js
@@ -42,3 +42,21 @@ function processArg(num) {
 }
 processed = processArg(7)
 // Only change code below this line
+
+function nextInLine(arr, item) {
+    // Only change code below this line
+    arr.push(item)
+    var array = arr.shift()
+    return array;
+    // Only change code above this line
+
+
+}
+
+// Setup
+var testArr = [1,2,3,4,5];
+
+// Display code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
