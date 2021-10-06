e2dcb4dabd41aa481cd1fc84bd3b9b900c906bcb
zamla312 <zamlazhekshenov@gmail.com>
2021-10-07 00:04:51 +0600

    task 162

--- src/pages/freeCodeCamp/task_6-10-2021.js
+++ src/pages/freeCodeCamp/task_6-10-2021.js
@@ -158,3 +158,17 @@ function testGreaterOrEqual(val) {
 }
 
 testGreaterOrEqual(10);
+
+function testLessThan(val) {
+    if (val < 25) {  // Change this line
+        return "Under 25";
+    }
+
+    if (val < 55) {  // Change this line
+        return "Under 55";
+    }
+
+    return "55 or Over";
+}
+
+testLessThan(10);
