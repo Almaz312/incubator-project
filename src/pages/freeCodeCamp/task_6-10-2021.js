4995bcf8baa437e0f00a279e0e3581cb3d593c51
zamla312 <zamlazhekshenov@gmail.com>
2021-10-06 21:52:31 +0600

    task 95

--- src/pages/freeCodeCamp/task_6-10-2021.js
+++ src/pages/freeCodeCamp/task_6-10-2021.js
@@ -90,3 +90,13 @@ function testEqual(val) {
 }
 
 testEqual(10);
+
+// Setup
+function testStrict(val) {
+    if (val === 7) { // Change this line
+        return "Equal";
+    }
+    return "Not Equal";
+}
+
+testStrict(10);
