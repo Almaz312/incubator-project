b0fe25b12a375b9d2259c2e8d83482e82bd30b52
zamla312 <zamlazhekshenov@gmail.com>
2021-10-07 00:00:06 +0600

    task 132

--- src/pages/freeCodeCamp/task_6-10-2021.js
+++ src/pages/freeCodeCamp/task_6-10-2021.js
@@ -130,3 +130,17 @@ function testStrictNotEqual(val) {
 }
 
 testStrictNotEqual(10);
+
+function testGreaterThan(val) {
+    if (val > 100) {  // Change this line
+        return "Over 100";
+    }
+
+    if (val > 10) {  // Change this line
+        return "Over 10";
+    }
+
+    return "10 or Under";
+}
+
+testGreaterThan(10);
