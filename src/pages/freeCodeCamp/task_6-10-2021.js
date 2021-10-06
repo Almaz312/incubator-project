fd5c8f7b291f62e32172d4ed55ed74779076aae2
zamla312 <zamlazhekshenov@gmail.com>
2021-10-06 21:48:58 +0600

    task 73

--- src/pages/freeCodeCamp/task_6-10-2021.js
+++ src/pages/freeCodeCamp/task_6-10-2021.js
@@ -69,3 +69,14 @@ function welcomeToBooleans() {
 
     // Only change code above this line
 }
+
+function trueOrFalse(wasThatTrue) {
+    // Only change code below this line
+    if (wasThatTrue) {
+        return "Yes, that was true";
+    }
+    return "No, that was false";
+
+    // Only change code above this line
+
+}
