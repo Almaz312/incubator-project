30c2f3534be6bdc2ec33df753e7c8ecfa6bbbc82
zamla312 <zamlazhekshenov@gmail.com>
2021-10-07 09:50:58 +0600

    task 29

--- src/pages/freeCodeCamp/task_7_10_2021.js
+++ src/pages/freeCodeCamp/task_7_10_2021.js
@@ -25,3 +25,15 @@ function testElseIf(val) {
 }
 
 testElseIf(7);
+
+function orderMyLogic(val) {
+    if (val < 5) {
+        return "Less than 5";
+    } else if (val < 10) {
+        return "Less than 10";
+    } else {
+        return "Greater than or equal to 10";
+    }
+}
+
+orderMyLogic(7);
