eab01543faeac67f3706fb3d9e5eb2980c84052d
zamla312 <zamlazhekshenov@gmail.com>
2021-10-07 22:51:02 +0600

    task 60

--- src/pages/freeCodeCamp/task_7_10_2021.js
+++ src/pages/freeCodeCamp/task_7_10_2021.js
@@ -55,3 +55,29 @@ function testSize(num) {
 }
 
 testSize(7);
+
+var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+function golfScore(par, strokes) {
+    // Only change code below this line
+    if (strokes === 1) {
+        return "Hole-in-one!"
+    } else if (strokes === 2) {
+        return "Eagle"
+    } else if (strokes === 3) {
+        return "Birdie"
+    } else if (par === 4 && strokes === 4) {
+        return "Par"
+    } else if (par ===4 && strokes ===5) {
+        return "Bogey"
+    } else if (par ===4 && strokes ===6) {
+        return "Double Bogey"
+    } else if (par >=4 && strokes >=7) {
+        return "Go Home!"
+    } else if (par ===5 && strokes ===5) {
+        return "Par"
+    }
+
+    // Only change code above this line
+}
+
+golfScore(5, 4);
