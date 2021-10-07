c4c8731e7ae500dda2bd845359edf300dac8cdc5
zamla312 <zamlazhekshenov@gmail.com>
2021-10-07 09:41:31 +0600

    task 17

--- src/pages/freeCodeCamp/task_7_10_2021.js
+++ src/pages/freeCodeCamp/task_7_10_2021.js
@@ -13,3 +13,15 @@ function testElse(val) {
 }
 
 testElse(4);
+
+function testElseIf(val) {
+    if (val > 10) {
+        return "Greater than 10";
+    } else if (val < 5) {
+        return "Smaller than 5";
+    } else {
+        return "Between 5 and 10";
+    }
+}
+
+testElseIf(7);
