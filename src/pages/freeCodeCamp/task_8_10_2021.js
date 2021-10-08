0e34e3117ca19fc1c5b3e182d7aa4ddd3fcca6fe
zamla312 <zamlazhekshenov@gmail.com>
2021-10-08 22:09:17 +0600

    task 27

--- src/pages/freeCodeCamp/task_8_10_2021.js
+++ src/pages/freeCodeCamp/task_8_10_2021.js
@@ -23,3 +23,11 @@ function chainToSwitch(val) {
 }
 
 chainToSwitch(7);
+
+function isLess(a, b) {
+    // Only change code below this line
+    return a < b;
+    // Only change code above this line
+}
+
+isLess(10, 15);
