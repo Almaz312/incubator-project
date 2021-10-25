535b2d4c7010e4e8e89808be75889bc58c6c0968
zamla312 <zamlazhekshenov@gmail.com>
2021-10-25 12:48:10 +0600

    add css files

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,48 @@
 import React from 'react';
 import './App.css';
-import {Technologies} from './pages/Technologies';
-import {Header} from "./pages/Header";
 
 const App = () => {
     return (
-        <div>
-            <Header/>
-            <Technologies/>
+        <div className="app-wrapper">
+            <header className="header">
+                <img src="https://www.tailorbrands.com/wp-content/uploads/2020/07/mcdonalds-logo.jpg"></img>
+            </header>
+            <nav className="nav">
+                <div>
+                    <a>Profile</a>
+                </div>
+                <div>
+                    <a>Massage</a>
+                </div>
+                <div>
+                    <a>Music</a>
+                </div>
+                <div>
+                    <a>News</a>
+                </div>
+                <div>
+                    <a>Settings</a>
+                </div>
+            </nav>
+            <div className="content">
+                <div>
+                    <img src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"></img>
+                </div>
+                <div>
+                    Ava   +    description
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        <div>post1</div>
+                        <div>post2</div>
+                        <div>post3</div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
