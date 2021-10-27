341f9c2dfe77b3d9e91a30297856279f3cda81db
zamla312 <zamlazhekshenov@gmail.com>
2021-10-27 16:14:18 +0600

    Add files

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,9 +1,34 @@
 import React from "react";
+import './Dialogs.css'
 
 export function Dialogs() {
     return (
-        <div>
-            Dialogs
+        <div className="dialogs">
+            <div className="dialogs-item">
+                <div className="dialog">
+                    Dimych
+                </div>
+                <div className="dialog">
+                    Esen
+                </div>
+                <div className="dialog">
+                    Aman
+                </div>
+                <div className="dialog">
+                    Arstan
+                </div>
+                <div className="dialog">
+                    Bakyt
+                </div>
+                <div className="dialog">
+                    Bob
+                </div>
+            </div>
+            <div className="massages">
+                <div className="massage">Hi</div>
+                <div className="massage">How are you</div>
+                <div className="massage">Fine</div>
+            </div>
         </div>
     )
 }
