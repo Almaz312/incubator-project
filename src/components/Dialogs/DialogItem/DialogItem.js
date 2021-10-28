584f06c09d6efc457f5b572f99b7b3afa0e16ac8
zamla312 <zamlazhekshenov@gmail.com>
2021-10-28 11:08:55 +0600

    add files and fix

---
+++ src/components/Dialogs/DialogItem/DialogItem.js
@@ -0,0 +1,12 @@
+import {NavLink} from "react-router-dom";
+import React from "react";
+import './../Dialogs.css'
+
+export function DialogItem(props) {
+    const path = "/dialogs/" + props.id;
+    return (
+        <div className="dialog">
+            <NavLink to={path}>{props.name}</NavLink>
+        </div>
+    );
+}
