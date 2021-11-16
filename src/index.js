4a0059360bbc1805cbe88f8102a3c2dca818d960
zamla312 <zamlazhekshenov@gmail.com>
2021-11-16 09:59:00 +0600

    add files

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import State from "./Redux/State";
-import {addPost} from "./Redux/State";
+import {rerenderEntireTree} from "./render";
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App state={State} addPost={addPost} />
-    </React.StrictMode>,
-    document.getElementById('root')
-);
 
+rerenderEntireTree(State)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
