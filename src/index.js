ef6d5c0c614369a90ad679889ac985ec5dfaac00
zamla312 <zamlazhekshenov@gmail.com>
2022-04-05 11:39:54 +0600

    add files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import reportWebVitals from './reportWebVitals';
 import State from "./Redux/State";
-import {rerenderEntireTree} from "./render";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {addPost, updateNewPostText} from "./Redux/State";
 
+export const rerenderEntireTree = (State) => {
+    ReactDOM.render(
+        <React.StrictMode>
+            <App state={State} addPost={addPost} updateNewPostText={updateNewPostText}/>
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+}
 
 rerenderEntireTree(State)
 // If you want to start measuring performance in your app, pass a function
